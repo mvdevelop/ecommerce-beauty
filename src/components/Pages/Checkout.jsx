b15c88f958e932290cb124196bdf1053832a7b74
Vinicius Dilly <marcosvmdilly@gmail.com>
2025-10-04 14:27:18 -0300

    working on checkout.jsx

--- src/components/Pages/Checkout.jsx
+++ src/components/Pages/Checkout.jsx
@@ -34,7 +34,59 @@ export default function Checkout() {
           <div className="col-lg-7">
             <h5>Contact</h5>
             <div className="mb-3">
-              <input type="text" className='form-control' />
+              <input type="text" className='form-control' placeholder='Email or mobile phone number' />
+            </div>
+            <div className="form-check mb-4">
+              <input type="checkbox" className='form-check-input' id='newsCheck' />
+              <label className='form-check-label' htmlFor="newsCheck">
+                Email me with news and offers
+              </label>
+            </div>
+            <h5>Delivery</h5>
+            <div>
+              <div className='mb-3'>
+                <div className='btn-group btn-from w-100' role='group'>
+                  <input 
+                    type="radio"
+                    className='btn-check'
+                    name='deliveryOption'
+                    id='ship'
+                    checked={deliveryOption === 'ship'}
+                    onChange={() => setDeliveryOption('ship')}
+                  />
+                  <label className='btn ship-btn' htmlFor="ship">Ship</label>
+
+                  <input 
+                    type="radio"
+                    className='btn-check'
+                    name='deliveryOption'
+                    id='pickup'
+                    checked={deliveryOption === 'pickup'}
+                    onChange={() => setDeliveryOption('pickup')}
+                  />
+                  <label className='btn pickup-btn' htmlFor="pickup">Pickup in store</label>
+                </div>
+              </div>
+
+              {deliveryOption === 'ship' && (
+                <div className="row mb-3">
+                  <div className="mb-3">
+                    <select className='form-select'>
+                      <option>Brazil</option>
+                      <option>Argentina</option>
+                      <option>Uruguay</option>
+                    </select>
+                  </div>
+                  <div className='col'>
+                    <input type="text" className='form-control' placeholder='First Name (optional)' />
+                  </div>
+                  <div className='col'>
+                    <input type="text" className='form-control' placeholder='Last Name (optional)' />
+                  </div>
+                </div>
+              )}
+
+              
             </div>
           </div>
         </div>
