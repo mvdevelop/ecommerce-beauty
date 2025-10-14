56075a7245e1a5b1107c291907d9148a8cd23a38
Vinicius Dilly <marcosvmdilly@gmail.com>
2025-10-14 12:05:08 -0300

    working on footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,13 @@
 
 import React from 'react';
 
+import payment01 from '../../assets/payment-1.svg';
+import payment02 from '../../assets/payment-2.svg';
+import payment03 from '../../assets/payment-3.svg';
+import payment04 from '../../assets/payment-4.svg';
+import payment05 from '../../assets/payment-5.svg';
+import payment06 from '../../assets/payment-6.svg'; 
+
 export default function Footer() {
   return (
     <>
@@ -77,6 +84,23 @@ export default function Footer() {
                     <i className='ri-facebook-circle-fill'></i>
                     <i className='ri-youtube-fill'></i>
                   </div>
+                </div>
+              </div>
+              <div className='col-lg-4'>
+                <div className='footer-logo text-center'>
+                  <a href="#" className="navbar-brand mx-auto order-0">
+                    <h2 className='m-0 fw-bold' style={{ letterSpacing: '2px'}}>BLUSHIFY</h2>
+                  </a>
+                </div>
+              </div>
+              <div className='col-lg-4'>
+                <div className='payment-img mt-4 mt-xl-0 d-flex gap-2 justify-content-center justify-content-lg-end'>
+                  <img src={payment01} alt="" />
+                  <img src={payment02} alt="" />
+                  <img src={payment03} alt="" />
+                  <img src={payment04} alt="" />
+                  <img src={payment05} alt="" />
+                  <img src={payment06} alt="" />
                 </div>
               </div>
             </div>
