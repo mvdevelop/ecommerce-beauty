f7baf1b558403ae402484273ce7a0b2c49f1d0a4
Vinicius Dilly <marcosvmdilly@gmail.com>
2025-10-16 15:20:02 -0300

    working on about.jsx css

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -10,6 +10,9 @@ import client03 from '../../assets/brand-logo-3.png';
 import client04 from '../../assets/brand-logo-4.png';
 import client05 from '../../assets/brand-logo-5.png';
 import client06 from '../../assets/brand-logo-6.png';
+import team01 from '../../assets/team-1.webp';
+import team02 from '../../assets/team-2.webp';
+import team03 from '../../assets/team-3.webp';
 
 const About = () => {
   const [ quote, setQuote ] = useState(
@@ -79,13 +82,80 @@ const About = () => {
           </div>
 
           <div className='row justify-content-center align-items-center mt-5 gy-4'>
-            <div className='col-6 col-sm-4 col-md-2 d-flex justify-content-center brands-img' onClick={() => setQuote('" Quality products at great prices, delivering the perfect mix of affordability and excellence. Each item ensures great value for money."')} style={{ cursor: 'pointer' }}>
-              <img src={client01} alt="Goodness" className='img-fluid' style={{ maxHeight: '60px', objectFit: 'contain' }}/>
+            <div className='col-6 col-sm-4 col-md-2 d-flex justify-content-center brands-img' onClick={() => setQuote('"Quality products at great prices, delivering the perfect mix of affordability and excellence. Each item ensures great value for money."')} style={{ cursor: 'pointer' }}>
+              <img src={client01} alt="Grand Golden Gallery" className='img-fluid' style={{ maxHeight: '60px', objectFit: 'contain' }}/>
             </div>
 
-            <div className='col-6 col-sm-4 col-md-2 d-flex justify-content-center brands-img' onClick={() => setQuote('" Great beauty service! The experience was truly amazing. and I feel more confident and radiant with my beauty after every visit."')} style={{ cursor: 'pointer' }}>
-              <img src={client02} alt="Grand Golden Gallery" className='img-fluid' style={{ maxHeight: '60px', objectFit: 'contain' }}/>
+            <div className='col-6 col-sm-4 col-md-2 d-flex justify-content-center brands-img' onClick={() => setQuote('"Great beauty service! The experience was truly amazing. and I feel more confident and radiant with my beauty after every visit."')} style={{ cursor: 'pointer' }}>
+              <img src={client02} alt="Parker & Co." className='img-fluid' style={{ maxHeight: '60px', objectFit: 'contain' }}/>
             </div>
+
+            <div className='col-6 col-sm-4 col-md-2 d-flex justify-content-center brands-img' onClick={() => setQuote('"Amazing product. The results are so transformative in texture and my face feels plump and healthy. Highly recommend!"')} style={{ cursor: 'pointer' }}>
+              <img src={client03} alt="The Beast" className='img-fluid' style={{ maxHeight: '60px', objectFit: 'contain' }}/>
+            </div>
+
+            <div className='col-6 col-sm-4 col-md-2 d-flex justify-content-center brands-img' onClick={() => setQuote('"Quality products at great prices, delivering the perfect mix of affordability and excellence. Each item ensures great value for money."')} style={{ cursor: 'pointer' }}>
+              <img src={client04} alt="The Hayden" className='img-fluid' style={{ maxHeight: '60px', objectFit: 'contain' }}/>
+            </div>
+
+            <div className='col-6 col-sm-4 col-md-2 d-flex justify-content-center brands-img' onClick={() => setQuote('"Great beauty service! The experience was truly amazing. and I feel more confident and radiant with my beauty after every visit."')} style={{ cursor: 'pointer' }}>
+              <img src={client05} alt="Good Mood" className='img-fluid' style={{ maxHeight: '60px', objectFit: 'contain' }}/>
+            </div>
+
+            <div className='col-6 col-sm-4 col-md-2 d-flex justify-content-center brands-img' onClick={() => setQuote('"Amazing product. The results are so transformative in texture and my face feels plump and healthy. Highly recommend!"')} style={{ cursor: 'pointer' }}>
+              <img src={client06} alt="Goodness" className='img-fluid' style={{ maxHeight: '60px', objectFit: 'contain' }}/>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Team Section */}
+      <section className='team-section'>
+        <h2 className='team-title'>
+          We pride ourselves on having a team of highly skilled
+        </h2>
+
+        <div className='team-row'>
+          <div className='team-member'>
+            <div className='team-image-wrapper'>
+              <img src={team01} alt="Slava Fedutik" className='team-image' />
+              <div className='team-social'>
+                <a href="#"><i className="ri-instagram-line"></i></a>
+                <a href="#"><i className="ri-twitter-line"></i></a>
+                <a href="#"><i className="ri-facebook-line"></i></a>
+                <a href="#"><i className="ri-youtube-line"></i></a>
+              </div>
+            </div>
+            <h3 className='team-name'>Slava Fedutik</h3>
+            <p className='team-role'>- Founder, Chief Creative</p>
+          </div>
+
+          <div className='team-member'>
+            <div className='team-image-wrapper'>
+              <img src={team02} alt="Jennifer C." className='team-image' />
+              <div className='team-social'>
+                <a href="#"><i className="ri-instagram-line"></i></a>
+                <a href="#"><i className="ri-twitter-line"></i></a>
+                <a href="#"><i className="ri-facebook-line"></i></a>
+                <a href="#"><i className="ri-youtube-line"></i></a>
+              </div>
+            </div>
+            <h3 className='team-name'>Jennifer C.</h3>
+            <p className='team-role'>- Founder, CEO</p>
+          </div>
+
+          <div className='team-member'>
+            <div className='team-image-wrapper'>
+              <img src={team03} alt="" className='team-image' />
+              <div className='team-social'>
+                <a href="#"><i className="ri-instagram-line"></i></a>
+                <a href="#"><i className="ri-twitter-line"></i></a>
+                <a href="#"><i className="ri-facebook-line"></i></a>
+                <a href="#"><i className="ri-youtube-line"></i></a>
+              </div>
+            </div>
+            <h3 className='team-name'>Valeria Nadopta</h3>
+            <p className='team-role'>- Founder, COO</p>
           </div>
         </div>
       </section>
