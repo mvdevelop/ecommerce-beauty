5f86f296d500d61d827cdc57b86f468ce34927ee
Vinicius Dilly <marcosvmdilly@gmail.com>
2025-10-17 11:00:39 -0300

    working on shop.jsx

--- src/components/Pages/Shop.jsx
+++ src/components/Pages/Shop.jsx
@@ -6,15 +6,33 @@ import 'react-toastify/dist/ReactToastify.css';
 
 // Data
 import productsData from '../../Product.json';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Shop() {
+
+  const [ filterSortOption, setFilterSortOption ] = useState('all');
+  const navigate = useNavigate();
+
+  const handleFilterSort = () => {
+    let filtered = [...productsData];
+
+    if (filterSortOption = 'new' || filterSortOption === 'sale') {} 
+  }
+
   return (
     <>
-      <ol className='sectopm-banner py-3 position-relative'>
+      <ol className='section-banner py-3 position-relative'>
         <li className='position-relative'><Link to='/'>Home</Link></li>
         <li className='position-relative active'><span className='ps-5'>Products</span></li>
       </ol>
+
+      <div className="shop-container">
+        <div className="container">
+          <h1 className="text-center py-4 fw-semibold">Products</h1>
+
+
+        </div>
+      </div>
     </>
   )
 }
