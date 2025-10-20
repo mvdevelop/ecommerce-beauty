9fab63b1e02c05917fa4e1c42c360a624e537949
Vinicius Dilly <marcosvmdilly@gmail.com>
2025-10-19 23:18:55 -0300

    working on blog.jsx css

--- src/components/Pages/Blog.jsx
+++ src/components/Pages/Blog.jsx
@@ -17,21 +17,34 @@ export default function Blog() {
         </li>
       </ol>
 
-      <div className='row'>
-        {blogs.map(blog => (
-          <div className='col-lg-4 col-md-6 mb-0'>
-            <div className='blog-items text-center position-relative'>
-              <div className="blog-image w-100 position-relative overflow-hidden">
-                <img src={blog.image} alt="blog-image" className='img-fluid' />
-              </div>
-              <div className='blog-content pt-3 w-100 position-relative'>
-                <div className='blog-title'>
-                  <span className='title'>{blog.title}</span>
+      <div className="container">
+        <h1 className='text-center py-4 fw-semibold'>News</h1>
+        <div className='row'>
+          {blogs.map(blog => (
+            <div className='col-lg-4 col-md-6 mb-0'>
+              <div className='blog-items text-center position-relative'>
+                <div className="blog-image w-100 position-relative overflow-hidden">
+                  <img src={blog.image} alt="blog-image" className='img-fluid' />
+                </div>
+                <div className='blog-content pt-3 w-100 position-relative'>
+                  <div className='blog-title'>
+                    <span className='title'>{blog.title}</span>
+                  </div>
+                  <h4>{blog.name}</h4>
+                  <ul className='py-3 d-flex w-100 text-center justify-content-center gap-2'>
+                    <li>
+                      By
+                      <span className='text-black ps-2'>{blog.author}</span>
+                    </li>
+                    <li>
+                      <span className='active position-relative ps-2'>{blog.date}</span>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </>
   )
